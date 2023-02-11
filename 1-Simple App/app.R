7028fb5393aa22d8089067395a4377e5742d2664
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 13:05:40 -0500

    Update app.R

--- 1-Simple App/app.R
+++ 1-Simple App/app.R
@@ -26,43 +26,46 @@ ui <- fluidPage(
 
     # Input: Specification of range within an interval
     sliderInput(
-      inputId = "range", label = "Range:",
-      min = 1, max = 1000,
+      inputId = "range",
+      label = "Range:",
+      min = 1,
+      max = 1000,
       value = c(200, 500)
     )
   ),
 
   # Body --------------------------------------
-  altPanel(card(
-    title = "Moveable card container #2",
-    "Move card, refresh and see that changes remain"
-  ),
-  card(
-    title = "Moveable card container",
-    # card body content
-    alert.text = "Realize that this card contains a status bar. More info at rpkg.net",
-    alert.bg = "danger",
-    "RPKG. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi bibendum neque egestas congue quisque egestas",
-    tags$br(),
-    tags$h4("Input slider for the image"),
-    plotOutput("sampleplot"),
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
-    )
-  ),
-  width = 12
+  altPanel(
+    card(
+      title = "Moveable card container A",
+      "This card is moveable. Try moving this card to the bottom. Refresh and see that changes remain. yay!"
+    ),
+    card(
+      title = "Moveable card container B",
+      # card body content
+      alert.text = "Realize that this card contains a status bar. More info at rpkg.net",
+      alert.bg = "danger",
+      "RPKG. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi bibendum neque egestas congue quisque egestas",
+      tags$br(),
+      tags$h4("Input slider for the image"),
+      plotOutput("sampleplot"),
+      sliderInput(
+        inputId = "bins",
+        label = "Number of bins:",
+        min = 1,
+        max = 50,
+        value = 30
+      )
+    ),
+    width = 12
   )
 )
 
 server <- function(input, output, session) {
-
   # make plots
   output$sampleplot <- renderPlot({
-    plot(x = sample(1:input$bins, 32, replace = TRUE), y = mtcars$hp)
+    plot(x = sample(1:input$bins, 32, replace = TRUE),
+         y = mtcars$hp)
   })
 }
 
