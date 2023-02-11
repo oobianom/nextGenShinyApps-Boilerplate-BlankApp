c9d07460e9de3539a31f97b344b2d813be73fcc2
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 12:48:31 -0500

    Update app.R

--- 1-Simple App/app.R
+++ 1-Simple App/app.R
@@ -1,51 +1,69 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a simple Shiny web application built using the nextGenShinyApps
 #
-# Find out more about building applications with Shiny here:
+# Find out more about building applications with the nextGenShinyApps here:
 #
-#    http://shiny.rstudio.com/
+#    https://nextgenshinyapps.obi.obianom.com
 #
 
+rm(list = ls(all = TRUE))
+
+# attach library
 library(shiny)
+library(nextGenShinyApps)
+library(shinyStorePlus)
 
-# Define UI for application that draws a histogram
+# Define UI
 ui <- fluidPage(
+  # Theme: Select color style from 1-13
+  style = "2",
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+  # Header: Insert header content using titlePanel ------------
+  header = titlePanel(left = "A Demo to Showcase the Features of the nextGenShinyApp R package", right = "Image logo"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+  # Sidebar: Insert content for sidebar ----------------------
+  sidebar = sidebarPanel(
+    title = "NextGenShinyApp",
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    # Input: Specification of range within an interval
+    sliderInput(
+      inputId = "range", label = "Range:",
+      min = 1, max = 1000,
+      value = c(200, 500)
     )
+  ),
+  card(
+    title = "Moveable card container",
+    # card body content
+    alert.text = "Realize that this card contains a status bar. More info at rpkg.net",
+    alert.bg = "danger",
+    "RPKG. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi bibendum neque egestas congue quisque egestas",
+    tags$br(),
+    tags$h4("Input slider for the image"),
+    plotOutput("sampleplot"),
+    sliderInput(
+      inputId = "bins",
+      label = "Number of bins:",
+      min = 1,
+      max = 50,
+      value = 30
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session){
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  # make plots
+  output$sampleplot <- renderPlot({
+    plot(x =sample(1:input$bins,32, replace = TRUE), y = mtcars$hp)
+  })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # include shinyStorePlus to track input
+  appid = "application501"
+  setupStorage(appId = appid,inputs = TRUE)
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+
+
+# Run the application
+shinyApp(ui, server)
